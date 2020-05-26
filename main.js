f645984ddafb6d63e0cd77d90af391e66ab62b95
Logan Jensen <logan.neil.jensen@gmail.com>
2020-05-26 18:48:12 -0500

    General fixes

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ app.set('port', process.argv[2]);
 
 ///////////////////////// UPDATE
 app.use('/', express.static('public'));
+app.use('/students', require('./students.js'));
 ///////////////////////// UPDATE
 
 
