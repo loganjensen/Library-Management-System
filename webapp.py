072f43455675c518c1a12760af97eb4e8c1821a6
Logan Jensen <logan.neil.jensen@gmail.com>
2020-05-26 20:15:03 -0500

    Removed database

--- webapp.py
+++ webapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask import request, redirect
-from db_connector.db_connector import connect_to_database, execute_query
+# from db_connector.db_connector import connect_to_database, execute_query
 #create the web application
 webapp = Flask(__name__)
 webapp.config['DEBUG'] = True
@@ -13,12 +13,12 @@ def homepage():
 
 @webapp.route('/students')
 def students():
-    db_connection = connect_to_database()
-    query = "SELECT first_name, last_name, email FROM Students;"
-    result = execute_query(db_connection, query).fetchall()
-    print(result)
+    # db_connection = connect_to_database()
+    # query = "SELECT first_name, last_name, email FROM Students;"
+    # result = execute_query(db_connection, query).fetchall()
+    # print(result)
 
-    return render_template('students.html', rows=result)
+    return render_template('students.html')
 
 @webapp.route('/books')
 def books():
