872bd9a508c9da162191cef100311629689579e4
Logan Jensen <logan.neil.jensen@gmail.com>
2020-06-09 20:33:50 -0500

    Rename routes

--- webapp.py
+++ webapp.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template
 from flask import request, redirect
 # from db_connector.db_connector import connect_to_database, execute_query
 #create the web application
-webapp = Flask(__name__)
-webapp.config['DEBUG'] = True
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
 #provide a route where requests on the web application can be addressed
-@webapp.route('/')
-@webapp.route('/index')
+@app.route('/')
+@app.route('/index')
 def homepage():
     return render_template('index.html')
 
-@webapp.route('/students')
+@app.route('/students')
 def students():
     # db_connection = connect_to_database()
     # query = "SELECT first_name, last_name, email FROM Students;"
@@ -20,10 +20,10 @@ def students():
 
     return render_template('students.html')
 
-@webapp.route('/books')
+@app.route('/books')
 def books():
     return render_template('books.html')
 
-@webapp.route('/booksonloan')
+@app.route('/booksonloan')
 def booksonloan():
     return render_template('booksonloan.html')
